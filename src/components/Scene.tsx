f588168968759674f82a77bcea06843f104951bb
afaankhawaja <aafaan02@gmail.com>
2025-09-02 23:29:29 +0500

    centered and scaled the scene for better model position

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,7 +1,8 @@
 import { Html, OrbitControls, useProgress } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useEffect, useRef } from "react";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { Box3, Vector3 } from "three";
 
 interface Props {
   modelUrl: string;
@@ -14,6 +15,23 @@ function Loader() {
 
 const Scene = ({ modelUrl }: Props) => {
   const gltf = useLoader(GLTFLoader, modelUrl);
+  const modelRef = useRef(null);
+
+  useEffect(() => {
+    if (!gltf.scene) return;
+
+    const box = new Box3().setFromObject(gltf.scene);
+    const center = box.getCenter(new Vector3());
+    const size = box.getSize(new Vector3());
+    const maxDim = Math.max(size.x, size.y, size.z);
+    const desiredMaxDim = 2;
+    const scaleFactor = desiredMaxDim / maxDim;
+
+    gltf.scene.position.sub(center);
+
+    gltf.scene.scale.setScalar(scaleFactor);
+
+  }, [gltf]);
 
   return (
     <div
@@ -32,7 +50,7 @@ const Scene = ({ modelUrl }: Props) => {
             height: "100%",
             display: "block",
           }}
-          camera={{ fov: 50 }}
+          camera={{ fov: 50, position: [0, 0, 5] }}
           shadows
         >
           <ambientLight intensity={0.4} />
@@ -42,9 +60,9 @@ const Scene = ({ modelUrl }: Props) => {
             intensity={1}
           />
           <primitive
+            ref={modelRef}
             object={gltf.scene}
-            position={[0, -5, 0]}
-            scale={[1, 1, 1]}
+            position={[0, 0, 0]}
           />
 
           <OrbitControls
