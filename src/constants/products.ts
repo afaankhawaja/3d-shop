bb58ba4fba19db04b93384df570cf55f3fc646d5
afaankhawaja <aafaan02@gmail.com>
2025-09-02 15:54:21 +0500

    updated the images and model path

--- src/constants/products.ts
+++ src/constants/products.ts
@@ -2,6 +2,11 @@ import tshirt from "../assets/images/t-shirt.png";
 import jacket from "../assets/images/jacket.png";
 import speaker from "../assets/images/speaker.png";
 import headphone from "../assets/images/headphone.png";
+import jean from "../assets/images/jean.png";
+import ledtv from "../assets/images/led-tv.png";
+import bookshelf from "../assets/images/bookshelf.png";
+import chair from "../assets/images/chair.png";
+import table from "../assets/images/table.png";
 
 export const products = [
   {
@@ -13,7 +18,7 @@ export const products = [
     imageUrl: headphone,
     price: 249.99,
     rating: 4.8,
-    modelUrl: "/models/leather-jacket/scene.gltf",
+    modelUrl: "/models/headphones/scene.gltf",
   },
   {
     id: 2,
@@ -21,10 +26,10 @@ export const products = [
     description:
       "Experience stunning clarity and vibrant colors with this 55-inch smart TV. Stream your favorite shows and movies in breathtaking 4K resolution.",
     category: "Electronics",
-    imageUrl: "https://images.unsplash.com/photo-1593784993175-9e67d3e09886",
+    imageUrl: ledtv,
     price: 699.0,
     rating: 4.5,
-    modelUrl: "/models/leather-jacket/scene.gltf",
+    modelUrl: "/models/led-tv/scene.gltf",
   },
   {
     id: 3,
@@ -35,7 +40,7 @@ export const products = [
     imageUrl: speaker,
     price: 79.5,
     rating: 4.6,
-    modelUrl: "/models/leather-jacket/scene.gltf",
+    modelUrl: "/models/bluetooth-speaker/scene.gltf",
   },
   {
     id: 4,
@@ -57,7 +62,7 @@ export const products = [
     imageUrl: tshirt,
     price: 25.0,
     rating: 4.7,
-    modelUrl: "/models/leather-jacket/scene.gltf",
+    modelUrl: "/models/t-shirt/scene.gltf",
   },
   {
     id: 6,
@@ -65,10 +70,10 @@ export const products = [
     description:
       "A comfortable and flattering fit that holds its shape all day. Made with premium stretch denim.",
     category: "Clothing",
-    imageUrl: "https://images.unsplash.com/photo-1560942548-d3c50970a253",
+    imageUrl: jean,
     price: 65.0,
     rating: 4.4,
-    modelUrl: "/models/leather-jacket/scene.gltf",
+    modelUrl: "/models/jeans/scene.gltf",
   },
   {
     id: 7,
@@ -76,10 +81,10 @@ export const products = [
     description:
       "A stylish and comfortable armchair that adds a touch of sophistication to any living space. Available in a variety of fabric options.",
     category: "Furniture",
-    imageUrl: "https://images.unsplash.com/photo-1586521995584-353d2673f45f",
+    imageUrl: chair,
     price: 399.0,
     rating: 4.8,
-    modelUrl: "/models/leather-jacket/scene.gltf",
+    modelUrl: "/models/armchair/scene.gltf",
   },
   {
     id: 8,
@@ -87,10 +92,10 @@ export const products = [
     description:
       "Crafted from solid oak with a clean, rustic design. The perfect centerpiece for your lounge.",
     category: "Furniture",
-    imageUrl: "https://images.unsplash.com/photo-1560965373-c820b1e1d003",
+    imageUrl: table,
     price: 150.0,
     rating: 4.5,
-    modelUrl: "/models/leather-jacket/scene.gltf",
+    modelUrl: "/models/coffee-table/scene.gltf",
   },
   {
     id: 9,
@@ -98,9 +103,9 @@ export const products = [
     description:
       "A sleek, minimalist bookshelf with ample storage. The perfect solution for organizing your books and decor.",
     category: "Furniture",
-    imageUrl: "https://images.unsplash.com/photo-1510103723321-b84784a0d879",
+    imageUrl: bookshelf,
     price: 220.0,
     rating: 4.7,
-    modelUrl: "/models/leather-jacket/scene.gltf",
+    modelUrl: "/models/bookshelf/scene.gltf",
   },
 ];
